6b1978d2c7ab76f2c4d491b6e6527139ae4fb38d
hwanginseong@laptop <3dlstjd3@gmail.com>
2024-09-21 23:44:13 +0900

    add movement

--- Drill1.py
+++ Drill1.py
@@ -1,1 +1,34 @@
 import turtle
+
+turtle.shape("turtle")
+
+def left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+
+turtle.onkey(left, 'a')
+
+turtle.onkey(right, 'd')
+
+turtle.onkey(up, 'w')
+
+turtle.onkey(down, 's')
+
+turtle.listen()
